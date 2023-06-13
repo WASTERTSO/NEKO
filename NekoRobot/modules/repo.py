f76f3b9113a28cb115566bb09cffe6b4246954f9
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-06-13 10:50:07 +0530

    Update repo.py

--- NekoRobot/modules/repo.py
+++ NekoRobot/modules/repo.py
@@ -1,3 +1,70 @@
+"""
+BSD 2-Clause License
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2022-2023, Awesome-Prince, [ https://github.com/Awesome-Prince]
+Copyright (c) 2022-2023, Programmer Network, [ https://github.com/Awesome-Prince/NekoRobot-3 ]
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+from telethon import Button
+
+from NekoRobot import tbot
+from NekoRobot.events import register
+
+PHOTO = "https://te.legra.ph/file/18e8d74e1d3d4b17581c8.mp4"
+
+
+@register(pattern=("/rreeescxmittepo"))
+async def awake(event):
+    NEKO = """
+         𝑆𝑜𝑜𝑛 !
+
+
+
+𝐷𝑒𝑣 ~ @ABOUT_YOUR_SHIV
+
+
+
+𝑆𝑢𝑝𝑝𝑜𝑟𝑡 ~ @TSO_CHATS
+
+
+
+𝑈𝑝𝑑𝑎𝑡𝑒𝑠 ~ @TSO_UPDATES
+
+
+
+
+
+
+
+
+
+"""
+
+    BUTTON = [
+        [
+            Button.url("𝐺𝑖𝑡𝒉𝑢𝑏", "GITHUB.COM/WASTERTSO"),
+            Button.url("𝑇𝑎𝑘𝑒", "https://te.legra.ph/file/87669dc5f87f29988ed40.mp4"),
+        ]
+    ]
+    await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
 
 
         
