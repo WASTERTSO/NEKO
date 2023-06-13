83d1f72087bd28bb1e153e1ebb1adaa4ebd88bff
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-06-13 10:43:24 +0530

    Update good morning.py

--- NekoRobot/modules/good morning.py
+++ NekoRobot/modules/good morning.py
@@ -31,7 +31,7 @@ from NekoRobot.events import register
 PHOTO = "https://te.legra.ph/file/7a18675abd9b75230735d.mp4"
 
 
-@register(pattern=("Good morning"))
+@register(pattern=("Gotrefmnrning"))
 async def awake(event):
     NEKO = f" Welcome this beautiful morning with a smile on your face. I hope youll have a great day today. Wishing you a very good morning! {event.sender.first_name}"
     BUTTON = [
