2add5acccf78505ce600f9f6802f992732f602bc
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-09-13 18:56:38 +0530

    Update backups.py

--- NekoRobot/modules/backups.py
+++ NekoRobot/modules/backups.py
@@ -393,7 +393,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "𝐵𝑎𝑐𝑘𝑢𝑝𝑠"
+__mod_name__ = "ʙᴀᴄᴋᴜᴘs"
 
 __help__ = """
 *Only for group owner:*
