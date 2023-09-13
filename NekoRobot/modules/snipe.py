bec95a5cd695d766355f607dd68581ab95c869c4
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-09-13 19:32:19 +0530

    Update snipe.py

--- NekoRobot/modules/snipe.py
+++ NekoRobot/modules/snipe.py
@@ -31,7 +31,7 @@ __help__ = """
 Make me send a message to a specific chat.
 """
 
-__mod_name__ = "𝑆𝑛𝑖𝑝𝑒"
+__mod_name__ = "sɴɪᴘᴇ"
 
 SNIPE_HANDLER = CommandHandler(
     "snipe", snipe, pass_args=True, filters=CustomFilters.dev_filter, run_async=True
