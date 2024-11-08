594c390bf762929b804af539f6c9c8fadbd9d1d1
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2024-11-08 21:29:15 +0530

    Update config.py

--- NekoRobot/config.py
+++ NekoRobot/config.py
@@ -149,7 +149,7 @@ class Config(object):
     AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
-    SYNC_TIME = True
+    sync_time = True
 
 class Production(Config):
     LOGGER = True
